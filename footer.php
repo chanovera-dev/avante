8adc9bdaf984c467f6506726903d437cf572649b
Chano Vera <github@chanovera.com>
2026-02-26 14:18:08 -0600

    First steps for footer

---
+++ footer.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * @package Avante
+ * @since Avante 1.0.0
+ */
+
+?>
+<footer id="main-footer"></footer>
+<?php wp_footer(); ?>
+</body>
+
+</html>
